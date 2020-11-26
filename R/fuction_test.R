5a773450c42935b64eba18611f6fcc44ec6c648a
DeivisonSouza <souzaflorestal@gmail.com>
2020-11-26 10:28:07 -0300

    function test

---
+++ R/fuction_test.R
@@ -0,0 +1,18 @@
+desc <-
+  function(x){
+    n <- length(x)
+    Soma <- sum(x)
+    Media <- round(sum(x)/n, digits=2)
+    Variancia <- var(x)
+    DP <- round(sd(x), digits=2)
+    CV <- round((DP/Media)*100,digits=2)
+    Minimo <- min(x)
+    Quartil1 <- quantile(x,0.25)
+    Mediana <- median(x)
+    Quartil3 <- quantile(x,0.75)
+    Interquatil <- Quartil3-Quartil1
+    Maximo <- max(x)
+    return(list(n=n, Media=Media, DP=DP, CV=CV, Min=Minimo,
+                Q1=Quartil1, Md=Mediana, Q3=Quartil3,
+                Interquatil=Interquatil, Max=Maximo))
+  }
