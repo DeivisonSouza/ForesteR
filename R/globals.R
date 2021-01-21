4c9fc523ecd0e9c336fadcafefd4256a3ccfccec
DeivisonSouza <souzaflorestal@gmail.com>
2021-01-21 11:50:32 -0300

    Add global variables

---
+++ R/globals.R
@@ -0,0 +1,3 @@
+utils::globalVariables(c("count", "N", "f", "E", "fp", "sd",
+                         "Value", "fc", "var", "VarM", "StdM",
+                         "ASE", "TotPop", "Parameters"))
