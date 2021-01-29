cccf3796879ed0b56434ffaddad5ea2c0b712bfe
DeivisonSouza <souzaflorestal@gmail.com>
2021-01-29 17:39:48 -0300

    Update SRS function and documentation

--- R/SRS.R
+++ R/SRS.R
@@ -1,13 +1,15 @@
 #' Stratified Random Sampling
 #'
-#' @param data Conjunto de dados que contém as informações de inventário florestal.
+#' @param data Conjunto de dados que contém as informações de inventário florestal para diferentes estratos.
 #' @param x Um vetor numérico com a variável de interesse.
-#' @param strata Fator de estratificação da característica de interesse.
-#' @param A Área total da população, em hectare.
-#' @param a Área da unidade de amostra, em hectare.
-#' @param SA Método de alocação das unidades de amostras nos estratos.
-#' @param FP Lógico. Fator de proporcionalidade para transformar para hectare. (default = FALSE)
-#' @param DT Lógico. Imprime a saída no painel de visualização. (default = FALSE)
+#' @param strata Fator. Usado para estratificação da variável de interesse.
+#' @param A Numérico. Um vetor númerico com a área total de cada estrato, em hectare.
+#' @param a Numérico. Área da unidade de amostra, em hectare.
+#' @param LE Numérico. Limite de erro admissível. O padrão é 0.1, isto é, 10% da média amostral estimada.
+#' @param SA Método de alocação das unidades de amostras nos estratos. Estão disponíveis os método "PA" (Proportional allocation) e "Neyman".
+#' @param FP Lógico. Fator de proporcionalidade para transformar os valores da variável de interesse para hectare. (default = FALSE)
+#' @param DT Lógico. Imprime as estimativas dos parâmetros no painel de visualização. (default = FALSE)
+#' @param digits Inteiro. Número de casas decimais para imprimir os resultados. (default = 3)
 #' @param ... Parâmetros adicionais
 #'
 #' @return Uma lista com estatística descritiva, anova, estimativas de amostragem aleatória estratificada e informações de base para cálculo das estimativas.
@@ -16,12 +18,12 @@
 #' @examples
 #' \dontrun{
 #' data(native)
-#' SRS(x=native$Volume, strata=native$Strata, A = c(650, 350), a = 1, SA = "PA")
+#' SRS(x=native$Volume, strata=native$Strata, A = c(650, 350), a = 1, LE = 0.1, SA = "PA")
 #'}
 #'
 #' @importFrom rlang .data
 
-SRS <- function(data = NULL, x, strata, A, a, SA = "PA", FP = FALSE, DT = FALSE, ...){
+SRS <- function(data = NULL, x, strata, A, a, LE = 0.1, SA = "PA", FP = FALSE, DT = FALSE, digits = 3, ...){
   #if(!is.data.frame(data)) stop("data must be a dataframe")
 
   if(!is.numeric(x) | is.integer(x) | is.character(x))
@@ -68,10 +70,12 @@ SRS <- function(data = NULL, x, strata, A, a, SA = "PA", FP = FALSE, DT = FALSE,
                      max = max(x),
                      kurt = moments::kurtosis(x),
                      skew = moments::skewness(x),
-                     .groups = 'drop')
+                     .groups = 'drop') #%>%
+    # dplyr::mutate_at(dplyr::vars(c(count,)), ceiling)
 
   tbDescBy <- descBy %>%
-    dplyr::mutate_if(is.numeric, format, scientific=FALSE) %>%
+    #dplyr::mutate_if(is.numeric, format, scientific=FALSE) %>%
+    dplyr::mutate(dplyr::across(where(is.numeric), round, digits)) %>%
     tidyr::pivot_longer(!strata,
                         names_to = "Parameters",
                         values_to = "Value") %>%
@@ -88,27 +92,27 @@ SRS <- function(data = NULL, x, strata, A, a, SA = "PA", FP = FALSE, DT = FALSE,
     dplyr::select(dplyr::all_of(c("strata", "count","Wh", "Nh", "Ah", "Mean", "var")))
 
   tbPar <- tbInf2 %>%
-    dplyr::summarise(E = 0.1*sum(Wh*Mean),
+    dplyr::summarise(E = LE*sum(Wh*Mean),
                      t = stats::qt(1-.05/2, length(x)-1),
                      #f = length(x)*a/sum(A),
-                     #f = length(x)/sum(Nh),
+                     f = length(x)/sum(Nh),
                      #f = (length(x)*(a*(1/a)))/sum(A),
-                     f = dplyr::case_when((FP == TRUE ) ~ (length(x)*(a*(1/a)))/sum(Ah),
-                                          (FP == FALSE) ~ length(x)/sum(Nh)),
+                     # f = dplyr::case_when((FP == TRUE ) ~ (length(x)*(a*(1/a)))/sum(Ah),
+                     #                      (FP == FALSE) ~ length(x)/sum(Nh)),
                      fc = 1-f,
-                     ne = dplyr::case_when((FP == TRUE ) ~ (sum(((Ah*(Ah-count))/count)*var)^2)/sum((((Ah*(Ah-count))/count)^2)*(var^2)/(count-1)),
-                                          (FP == FALSE) ~ (sum(((Nh*(Nh-count))/count)*var)^2)/sum((((Nh*(Nh-count))/count)^2)*(var^2)/(count-1))),
-                     #ne = (sum(((Nh*(Nh-count))/count)*var)^2)/sum((((Nh*(Nh-count))/count)^2)*(var^2)/(count-1)),
+                     # ne = dplyr::case_when((FP == TRUE ) ~ (sum(((Ah*(Ah-count))/count)*var)^2)/sum((((Ah*(Ah-count))/count)^2)*(var^2)/(count-1)),
+                     #                      (FP == FALSE) ~ (sum(((Nh*(Nh-count))/count)*var)^2)/sum((((Nh*(Nh-count))/count)^2)*(var^2)/(count-1))),
+                     ne = (sum(((Nh*(Nh-count))/count)*var)^2)/sum((((Nh*(Nh-count))/count)^2)*(var^2)/(count-1)),
                      N = sum(Nh),
                      Count = sum(count),
                      n = dplyr::case_when((fc >= 0.98 & SA == "PA") ~ (t^2*sum(Wh*var))/ E^2, # infinite
-                                           (fc < 0.98 & SA == "PA") ~ (t^2*sum(Wh*var))/ ((E^2)+((t^2)*(sum(Wh*var/ifelse(FP == FALSE, sum(Nh), sum(Ah)))))), # finite
+                                           (fc < 0.98 & SA == "PA") ~ (t^2*sum(Wh*var))/ ((E^2)+((t^2)*(sum(Wh*var/sum(Nh))))), # finite
                                            (fc >= 0.98 & SA == "Neyman") ~ (t^2*(sum(Wh*sqrt(var))^2))/ E^2, # infinite
-                                           (fc < 0.98 & SA == "Neyman") ~ (t^2*(sum(Wh*sqrt(var))^2))/ ((E^2)+((t^2)*(sum(Wh*var/ifelse(FP == FALSE, sum(Nh), sum(Ah)))))) # finite
+                                           (fc < 0.98 & SA == "Neyman") ~ (t^2*(sum(Wh*sqrt(var))^2))/ ((E^2)+((t^2)*(sum(Wh*var/sum(Nh))))) # finite
                                           ),
                      mean.st = sum(Wh*Mean),
                      var.st = sum(Wh*var),
-                     var.mean.st = sum(Wh^2*(var/count)) - sum((Wh*var)/ifelse(FP == FALSE, sum(Nh), sum(Ah))),
+                     var.mean.st = sum(Wh^2*(var/count)) - sum((Wh*var)/sum(Nh)),
                      std.mean.st = sqrt(var.mean.st),
                      ase = stats::qt(1-.05/2, ne)*std.mean.st,
                      rse = (ase/mean.st)*100,
@@ -118,9 +122,11 @@ SRS <- function(data = NULL, x, strata, A, a, SA = "PA", FP = FALSE, DT = FALSE,
                      lcip = totPop - ifelse(FP == FALSE, sum(Nh), sum(Ah))*(ase),
                      ucip = totPop + ifelse(FP == FALSE, sum(Nh), sum(Ah))*(ase)
                      ) %>%
-    dplyr::mutate_if(is.numeric, format, scientific=FALSE)
+    dplyr::mutate(dplyr::across(where(is.numeric), round, digits))
+    #dplyr::mutate_at(dplyr::vars(-c(N, Count)), round, digits)
+    #dplyr::mutate_if("Parameters" %in% c("N", "Count"), ceiling)
 
-  tbPar <- tbPar %>%
+    tbPar <- tbPar %>%
     tidyr::pivot_longer(tidyselect::everything(),
                         names_to = 'Parameters',
                         values_to = 'Value')
@@ -157,3 +163,8 @@ SRS <- function(data = NULL, x, strata, A, a, SA = "PA", FP = FALSE, DT = FALSE,
   return(result)
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 }
+
+# Teste FP (dados: http://www.mensuracaoflorestal.com.br/capitulo-5-amostragem-casual-estratificada)
+# r <- SRS(x=test$Volume, strata=test$Strata, A = c(14.4, 16.4, 14.2), a = 0.1, LE = 0.05, SA = "PA", FP = TRUE)
+# z <- SRS(x=test$Volume, strata=test$Strata, A = c(14.4, 16.4, 14.2), a = 0.1, LE = 0.05, SA = "PA", FP = FALSE)
+
