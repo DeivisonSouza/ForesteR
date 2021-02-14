db5d23da1528f94e455a1a878a23119947384b22
DeivisonSouza <souzaflorestal@gmail.com>
2021-02-14 11:46:37 -0300

    Add new global variables

--- R/globals.R
+++ R/globals.R
@@ -1,6 +1,6 @@
 utils::globalVariables(c("count", "N", "f", "E", "fp", "sd",
                          "Value", "fc", "var", "VarM", "StdM",
                          "ASE", "TotPop", "Parameters", ".",
-                         "Ah", "Wh", "Mean", "Nh", "var.mean.st",
-                         "ne", "std.mean.st", "ase", "mean.st",
+                         "Ah", "Wh", "Mean", "Nh", "var.mean", "var.mean.st",
+                         "ne", "std.mean", "std.mean.st", "ase", "mean.st",
                          "totPop", "var.mean.st", "where"))
